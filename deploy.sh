113c8b1798f5fd86f50d6b3cd7100357be34ba3c
tiii <gh@tiii.de>
2016-09-22 15:52:57 +0200

    do not finally?

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-rm -rf .git/
-
-
 cd build
-git remote add origin git@github.com:mau-mau-ch/mau-mau-ch.github.io.git
-git fetch
-
+git init
 git config user.email "deployment@mau-mau.ch"
 git config user.name "Magical Deployment Bot"
-git add .
+git remote add origin git@github.com:mau-mau-ch/mau-mau-ch.github.io.git
 
-git branch -u origin/master
+git fetch
+git reset --hard origin/master
+
+cd ..
+bundle exec middleman build
+cd build
+
+git add .
 
 git commit -m "Site update `date -u "+%Y.%m.%d-%H:%M:%S"`"
 git push --force --set-upstream origin master
