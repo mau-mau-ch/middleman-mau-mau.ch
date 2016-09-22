942259281d2cfd46626c16b528259e5ef9bd6bd6
tiii <gh@tiii.de>
2016-09-22 15:41:44 +0200

    do not initialize

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+rm -rf .git/
+
+
 cd build
-git init
+git remote add origin git@github.com:mau-mau-ch/mau-mau-ch.github.io.git
+git fetch
+
 git config user.email "deployment@mau-mau.ch"
 git config user.name "Magical Deployment Bot"
 git add .
 
-git remote add origin git@github.com:mau-mau-ch/mau-mau-ch.github.io.git
 git branch -u origin/master
-git fetch
 
-git commit --allow-empty -m "Site update `date -u "+%Y.%m.%d-%H:%M:%S"`"
+git commit -m "Site update `date -u "+%Y.%m.%d-%H:%M:%S"`"
 git push --force --set-upstream origin master
