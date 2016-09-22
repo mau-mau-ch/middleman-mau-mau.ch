1e8bf58096fe5afb3b10eba93e3da5bf4a700324
tiii <gh@tiii.de>
2016-09-22 14:56:19 +0200

    add git info to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 
 cd build
 git init
+git config user.email "deployment@mau-mau.ch"
+git config user.name "Magical Deployment Bot"
 git remote add origin git@github.com:mau-mau-ch/mau-mau-ch.github.io.git
 git commit --allow-empty -am "Site update `date -u "+%Y.%m.%d-%H:%M:%S"`"
+git push --set-upstream origin master
 git push -f
