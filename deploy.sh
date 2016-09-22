e020f276cbeba37fa682877669993fa25457692e
tiii <gh@tiii.de>
2016-09-22 15:38:17 +0200

    add

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@ cd build
 git init
 git config user.email "deployment@mau-mau.ch"
 git config user.name "Magical Deployment Bot"
+git add .
 
 git remote add origin git@github.com:mau-mau-ch/mau-mau-ch.github.io.git
 git branch -u origin/master
 git fetch
 
-git commit --allow-empty -am "Site update `date -u "+%Y.%m.%d-%H:%M:%S"`"
+git commit --allow-empty -m "Site update `date -u "+%Y.%m.%d-%H:%M:%S"`"
 git push --force --set-upstream origin master
